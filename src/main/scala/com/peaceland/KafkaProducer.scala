e59e1f01478cbc14aadaf537ec38059ef2565a52
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-07-18 10:23:24 +0200

    ADD: load csv

--- src/main/scala/com/peaceland/KafkaProducer.scala
+++ src/main/scala/com/peaceland/KafkaProducer.scala
@@ -2,8 +2,10 @@ package com.peaceland
 
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-
+import org.apache.spark
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
 import org.apache.spark.sql.SQLContext
+import org.apache.spark._
 import org.apache.spark.{SparkConf, SparkContext}
 
 object KafkaProducerApp extends App {
@@ -17,7 +19,27 @@ object KafkaProducerApp extends App {
   props.put("acks","all")
   val producer = new KafkaProducer[String, String](props)
   val topic = "report"
+
   try {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    val df = spark.read.csv("data/drone.csv")
+
+    df.printSchema()
+
+    def printRowInfos(row: Row): Unit =
+    {
+      printf(s"sent record %s %s %s %s %s %s\n",
+        row(0), row(1), row(2), row(3), row(4), row(5))
+    }
+
+    df.rdd.foreach(printRowInfos)
+
     for (i <- 0 to 15) {
       val record = new ProducerRecord[String, String](topic, i.toString, "tuer autre humain" + i)
       val metadata = producer.send(record)
